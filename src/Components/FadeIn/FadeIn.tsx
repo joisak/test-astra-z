9667675549b9a7c7d6b0a92fda5aa5235daa68e8
joakim isaksson <joakim.isaksson@consid.se>
2023-04-04 12:13:30 +0200

    scroll up when linking to show show

--- src/Components/FadeIn/FadeIn.tsx
+++ src/Components/FadeIn/FadeIn.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import styles from './FadeIn.module.scss';
 
 interface props {
@@ -6,6 +7,11 @@ interface props {
 }
 
 const FadeIn = ({ children }: props) => {
+	const { pathname } = useLocation();
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+
 	return <div className={styles.root}>{children}</div>;
 };
 
